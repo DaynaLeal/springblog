7987aa5270ab5e5e73898e13e47b361a57603283
Dayna Leal <dayna.r.leal@gmail.com>
2020-03-16 23:47:57 -0500

    post constructor created with getters and setters

---
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -0,0 +1,42 @@
+package com.codeup.springblog.models;
+
+public class Post {
+    private long id;
+    private String title;
+    private String body;
+
+
+    //constructor
+    public Post(long id, String title, String body){
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    //getters and setters
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
